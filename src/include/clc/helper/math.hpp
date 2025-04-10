5dd50fdfb99a82bf8b7cf05a08106378e0d36dc6
lumina37 <starry.qvq@gmail.com>
2025-04-10 15:53:26 +0800

    fix: missing namespace

--- src/include/clc/helper/math.hpp
+++ src/include/clc/helper/math.hpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <concepts>
 
+namespace clc {
 template <std::unsigned_integral Tv>
 [[nodiscard]] static constexpr inline bool isPowOf2(const Tv v) noexcept {
     return (v & (v - 1)) == 0;
@@ -11,4 +12,6 @@ template <std::unsigned_integral Tv>
 template <std::integral Tv>
 [[nodiscard]] static constexpr inline Tv alignUp(const Tv v, const size_t to) noexcept {
     return (v + (to - 1)) & ((~to) + 1);
-};
+}
+
+}  // namespace clc
